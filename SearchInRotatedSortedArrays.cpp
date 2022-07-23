dbeb2029f9673f8662e00833543b3ecd862ec44a
Adarsh Chaturvedi <83339750+a-chb@users.noreply.github.com>
2022-07-24 02:57:25 +0530

    Update SearchInRotatedSortedArrays.cpp

--- SearchInRotatedSortedArrays.cpp
+++ SearchInRotatedSortedArrays.cpp
@@ -1,3 +1,21 @@
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+    
+    
+
 /*---------------------------------------Solution---------------------------------------*/
 
 Runtime: 19 ms, faster than 5.15% of C++ online submissions for Search in Rotated Sorted Array.
