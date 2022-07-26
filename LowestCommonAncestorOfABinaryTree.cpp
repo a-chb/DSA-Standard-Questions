5291a69334f9ae3d9cee9a5f061ea2ee51f173ac
Adarsh Chaturvedi <83339750+a-chb@users.noreply.github.com>
2022-07-26 15:37:00 +0530

    Create LowestCommonAncestorOfABinaryTree.cpp

---
+++ LowestCommonAncestorOfABinaryTree.cpp
@@ -0,0 +1,55 @@
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+ 
+
+Example 1:
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+
+Example 2:
+
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+
+Example 3:
+
+Input: root = [1,2], p = 1, q = 2
+Output: 1
+
+  /*--------------------------------Solution--------------------------------*/
+  
+Solution in O(N) time && O(N) space.
+Runtime: 21 ms, faster than 70.31% of C++ online submissions for Lowest Common Ancestor of a Binary Tree.
+Memory Usage: 14.3 MB, less than 57.81% of C++ online submissions for Lowest Common Ancestor of a Binary Tree.
+  
+
+  /*--------------------------------Code Begins--------------------------------*/
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL || root == p || root == q) {
+            return root;
+        }
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        //result
+        if(left == NULL) {
+            return right;
+        }
+        else if(right == NULL) {
+            return left;
+        }
+        else { //both left and right are not null, we found our result
+            return root;
+        }
+        
+    }
+};
+ /*--------------------------------Code Ends--------------------------------*/
