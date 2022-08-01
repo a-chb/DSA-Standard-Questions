da2ae781ae9155e8ee06eaf0af8a79d721b292f7
Adarsh Chaturvedi <83339750+a-chb@users.noreply.github.com>
2022-08-01 18:03:53 +0530

    Update UniquesPaths.cpp

--- UniquesPaths.cpp
+++ UniquesPaths.cpp
@@ -1,7 +1,32 @@
+
+
+
+
+||==================================================>Solution<==================================================||
+
 Solution using tabulation in optimal Space
 TC=O(n) and SC = (n*m)
 
-Runtime: 4 ms, faster than 28.94% of C++ online submissions for Unique Paths.
-Memory Usage: 6.6 MB, less than 26.54% of C++ online submissions for Unique Paths.
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Unique Paths.
+Memory Usage: 6.5 MB, less than 26.54% of C++ online submissions for Unique Paths.
   
-  
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+       vector<vector<int>>dp(m , vector<int>(n , -1));
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+         if(i==0 && j==0)  dp[i][j]=1;
+         else{
+             int up=0,down=0;
+             if(i>0)   up   = dp[i-1][j];
+             if(j>0)   down = dp[i][j-1];
+             dp[i][j] = up+down;
+            }
+        }
+    }
+    return dp[m-1][n-1];
+    }
+};
