1a3037420cf805a6d2ba640c68114d059d7d0b65
Adarsh Chaturvedi <83339750+a-chb@users.noreply.github.com>
2022-08-06 22:44:23 +0530

    Update PoorPigs.cpp

--- PoorPigs.cpp
+++ PoorPigs.cpp
@@ -1,4 +1,6 @@
-There are buckets buckets of liquid, where exactly one of the buckets is poisonous. To figure out which one is poisonous, you feed some number of (poor) pigs the liquid to see whether they will die or not. Unfortunately, you only have minutesToTest minutes to determine which bucket is poisonous.
+There are buckets buckets of liquid, where exactly one of the buckets is poisonous. 
+To figure out which one is poisonous, you feed some number of (poor) pigs the liquid to see whether they will die or not. 
+Unfortunately, you only have minutesToTest minutes to determine which bucket is poisonous.
 
 You can feed the pigs according to these steps:
 
@@ -9,17 +11,18 @@ After minutesToDie minutes have passed, any pigs that have been fed the poisonou
 Repeat this process until you run out of time.
 Given buckets, minutesToDie, and minutesToTest, return the minimum number of pigs needed to figure out which bucket is poisonous within the allotted time.
 
- 
-
 Example 1:
-
 Input: buckets = 1000, minutesToDie = 15, minutesToTest = 60
 Output: 5
-Example 2:
 
+Example 2:
 Input: buckets = 4, minutesToDie = 15, minutesToTest = 15
 Output: 2
 
+ 
+ ||========================================>Solution<========================================||
+ 
+ 
 Runtime: 2 ms, faster than 45.09% of C++ online submissions for Poor Pigs.
 Memory Usage: 6 MB, less than 80.73% of C++ online submissions for Poor Pigs.
   
