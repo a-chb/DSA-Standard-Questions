91916cfd1cc7ed5993c5955ee61aa7c4c898999b
Adarsh Chaturvedi <83339750+a-chb@users.noreply.github.com>
2022-08-09 23:35:54 +0530

    Update VerifyingAnAlienDictionary.cpp

--- VerifyingAnAlienDictionary.cpp
+++ VerifyingAnAlienDictionary.cpp
@@ -1,1 +1,24 @@
-
+||================================================>Solution<================================================||
+  
+  Approch: 1 Iterrative
+    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Verifying an Alien Dictionary.
+    Memory Usage: 9.5 MB, less than 76.20% of C++ online submissions for Verifying an Alien Dictionary.
+      
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        for (int i = 0; i < words.size() - 1; i++) {
+        string word1 = words[i];
+        string word2 = words[i + 1];
+        int i1 = 0, i2 = 0;
+        while (word1[i1] == word2[i2]) {
+            i1++, i2++;
+        }
+        int r = order.find(word1[i1]);   
+        int s = order.find(word2[i2]);
+        if (r > s) return false;
+    }
+    return true;
+        
+    }
+};
