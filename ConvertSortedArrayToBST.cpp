89433c791df6806c573491aefa908fbbf786cd6c
Adarsh Chaturvedi <83339750+a-chb@users.noreply.github.com>
2022-08-10 16:39:49 +0530

    Update ConvertSortedArrayToBST.cpp

--- ConvertSortedArrayToBST.cpp
+++ ConvertSortedArrayToBST.cpp
@@ -1,1 +1,56 @@
+Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
 
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+  
+Example 1:
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+Example 2:
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+  
+
+||====================================>Solution<====================================||
+
+
+Runtime: 34 ms, faster than 19.73% of C++ online submissions for Convert Sorted Array to Binary Search Tree.
+Memory Usage: 24 MB, less than 17.10% of C++ online submissions for Convert Sorted Array to Binary Search Tree.
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& num) {
+        
+      if(num.size() == 0) return NULL;
+        if(num.size() == 1)
+        {
+            return new TreeNode(num[0]);
+        }
+        
+        int middle = num.size()/2;
+        TreeNode* root = new TreeNode(num[middle]);
+        
+        vector<int> leftInts(num.begin(), num.begin()+middle);
+        vector<int> rightInts(num.begin()+middle+1, num.end());
+        
+        root->left = sortedArrayToBST(leftInts);
+        root->right = sortedArrayToBST(rightInts);
+        
+        return root;
+        
+    }
+};
