10682f9cb9ca5f7ba79f6bc2d5d5b0e5a8a6a146
Adarsh Chaturvedi <83339750+a-chb@users.noreply.github.com>
2022-08-13 15:36:05 +0530

    Update UniqueBinarySearchTrees.cpp

--- UniqueBinarySearchTrees.cpp
+++ UniqueBinarySearchTrees.cpp
@@ -1,23 +1,45 @@
-// class Solution {
-// public:
-//     int numTrees(int n) {
-//         long ans = 1;
-//         for(int i = 0; i < n; i++) 
-//             ans *= (4*i+2) / (i+2.);
-//         return ans;
-//     }
-// };
+Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n.
 
-// class Solution {
-// public:
-//     int numTrees(int n) {
-//         if(n <= 1) return 1;
-//         int ans = 0;
-//         for(int i = 1; i <= n; i++) 
-//             ans += numTrees(i-1) * numTrees(n-i);
-//         return ans;
-//     }
-// };
+Example 1:
+Input: n = 3
+Output: 5
+    
+Example 2:
+Input: n = 1
+Output: 1
+
+
+||==============================>Solution<==============================||
+
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Unique Binary Search Trees.
+Memory Usage: 5.9 MB, less than 65.59% of C++ online submissions for Unique Binary Search Trees.
+
+Approch: 01 Brutforce
+
+class Solution {
+public:
+    int numTrees(int n) {
+        long ans = 1;
+        for(int i = 0; i < n; i++) 
+            ans *= (4*i+2) / (i+2.);
+        return ans;
+    }
+};
+
+Approch:02 Using Calatan Numbers TC -> O(N)
+
+class Solution {
+public:
+    int numTrees(int n) {
+        if(n <= 1) return 1;
+        int ans = 0;
+        for(int i = 1; i <= n; i++) 
+            ans += numTrees(i-1) * numTrees(n-i);
+        return ans;
+    }
+};
+
+Approch: 03 Dynamic Programming TC -> O(N^2)
 
 class Solution {
 public:
