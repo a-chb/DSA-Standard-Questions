999fef682e485dcff829dc882ada3cf9c10126eb
Adarsh Chaturvedi <83339750+a-chb@users.noreply.github.com>
2022-08-23 21:41:03 +0530

    Update MiddleOfTheLinkedList.cpp

--- MiddleOfTheLinkedList.cpp
+++ MiddleOfTheLinkedList.cpp
@@ -1,8 +1,21 @@
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+Example 2:
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
 ||========================================>Solution<========================================||
     Runtime: 2 ms, faster than 63.16% of C++ online submissions for Middle of the Linked List.
     Memory Usage: 7.2 MB, less than 22.54% of C++ online submissions for Middle of the Linked List
  
- Approch: Floyd Algorithm   
+ Approch: Tortoise and Hare approch  
  
  /**
  * Definition for singly-linked list.
