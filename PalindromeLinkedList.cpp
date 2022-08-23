7b88435e508a1b738a05b71a09d624a4b9c6dd90
Adarsh Chaturvedi <83339750+a-chb@users.noreply.github.com>
2022-08-23 21:07:03 +0530

    Update PalindromeLinkedList.cpp

--- PalindromeLinkedList.cpp
+++ PalindromeLinkedList.cpp
@@ -1,3 +1,14 @@
+Given the head of a singly linked list, return true if it is a palindrome.
+
+Example 1:
+Input: head = [1,2,2,1]
+Output: true
+
+Example 2:
+Input: head = [1,2]
+Output: false
+
+
 ||========================================>Solution<========================================||
   
   Approch: 1 Brutforce O(N) Time && O(N) Space.
