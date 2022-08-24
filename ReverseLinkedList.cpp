a62d758493371bf5c6825dd8a4fef9d4b619dcc4
Adarsh Chaturvedi <83339750+a-chb@users.noreply.github.com>
2022-08-24 21:14:25 +0530

    Update ReverseLinkedList.cpp

--- ReverseLinkedList.cpp
+++ ReverseLinkedList.cpp
@@ -1,3 +1,17 @@
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+
 ||========================================>Solution<========================================||
 
     Runtime: 10 ms, faster than 54.76% of C++ online submissions for Reverse Linked List.
