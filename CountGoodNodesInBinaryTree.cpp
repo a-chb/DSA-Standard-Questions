93ac642908a11871526146377d7c250010c83069
Adarsh Chaturvedi <83339750+a-chb@users.noreply.github.com>
2022-09-01 15:58:35 +0530

    Create CountGoodNodesInBinaryTree.cpp

---
+++ CountGoodNodesInBinaryTree.cpp
@@ -0,0 +1,2 @@
+Runtime: 218 ms, faster than 42.61% of C++ online submissions for Count Good Nodes in Binary Tree.
+Memory Usage: 86.4 MB, less than 54.67% of C++ online submissions for Count Good Nodes in Binary Tree.
