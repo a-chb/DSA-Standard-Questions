b20c71f25184e5e098cfb74313ca219645e6e7b8
Adarsh Chaturvedi <83339750+a-chb@users.noreply.github.com>
2022-09-01 16:03:34 +0530

    Update CountGoodNodesInBinaryTree.cpp

--- CountGoodNodesInBinaryTree.cpp
+++ CountGoodNodesInBinaryTree.cpp
@@ -1,2 +1,46 @@
-Runtime: 218 ms, faster than 42.61% of C++ online submissions for Count Good Nodes in Binary Tree.
-Memory Usage: 86.4 MB, less than 54.67% of C++ online submissions for Count Good Nodes in Binary Tree.
+Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+Return the number of good nodes in the binary tree.
+
+Example 1:
+Input: root = [3,1,4,3,null,1,5]
+Output: 4
+Explanation: Nodes in blue are good.
+Root Node (3) is always a good node.
+Node 4 -> (3,4) is the maximum value in the path starting from the root.
+Node 5 -> (3,4,5) is the maximum value in the path
+Node 3 -> (3,1,3) is the maximum value in the path.
+
+Example 2:
+Input: root = [3,3,null,4,2]
+Output: 3
+Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+
+Example 3:
+Input: root = [1]
+Output: 1
+Explanation: Root is considered as good.
+
+||====================================================>Solution<====================================================||
+
+    Runtime: 218 ms, faster than 42.61% of C++ online submissions for Count Good Nodes in Binary Tree.
+    Memory Usage: 86.4 MB, less than 54.67% of C++ online submissions for Count Good Nodes in Binary Tree.
+      
+class Solution {
+public:
+    int ans = 0;
+    void countNodes(TreeNode* root, int max){
+        if(root==NULL) return;
+        if(root->val >= max){
+            ans++;
+            max = root->val;
+        }
+        countNodes(root->left, max);
+        countNodes(root->right, max);
+    }
+    int goodNodes(TreeNode* root) {
+        int max = INT_MIN;
+        countNodes(root,max);
+        return ans;
+    }
+};     
+ 
