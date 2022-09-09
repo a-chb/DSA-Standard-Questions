898ec62560a7195bf3e036aafa1edb7626ecd3ad
Adarsh Chaturvedi <83339750+a-chb@users.noreply.github.com>
2022-09-09 15:46:10 +0530

    Update MaximumDepthofBinaryTree.cpp

--- MaximumDepthofBinaryTree.cpp
+++ MaximumDepthofBinaryTree.cpp
@@ -3,3 +3,13 @@
 Runtime: 17 ms, faster than 36.94% of C++ online submissions for Maximum Depth of Binary Tree.
 Memory Usage: 18.8 MB, less than 89.27% of C++ online submissions for Maximum Depth of Binary Tree.
   
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root) return 0;
+        int maxLeft = maxDepth(root->left);
+        int maxRight = maxDepth(root->right);
+        return max(maxLeft, maxRight)+1;
+    }
+};
+  
