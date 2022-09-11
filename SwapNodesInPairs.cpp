ae5f8bbfafd368a0cfa6df2ab1871211023873cd
Adarsh Chaturvedi <83339750+a-chb@users.noreply.github.com>
2022-09-11 13:43:23 +0530

    Update SwapNodesInPairs.cpp

--- SwapNodesInPairs.cpp
+++ SwapNodesInPairs.cpp
@@ -1,1 +1,15 @@
+||============================================>Soution<============================================||
 
+    Runtime: 0 ms, faster than 100.00% of C++ online submissions for Swap Nodes in Pairs.
+    Memory Usage: 7.6 MB, less than 6.71% of C++ online submissions for Swap Nodes in Pairs.
+      
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head != NULL && head->next != NULL){
+            swap(head->val,head->next->val);
+            swapPairs(head->next->next);
+        }
+        return head;
+    }
+};
