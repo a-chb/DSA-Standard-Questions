d4167b67a11799f99c8ae1a93b85c2a6927d6612
Adarsh Chaturvedi <83339750+a-chb@users.noreply.github.com>
2022-10-10 11:03:52 +0530

    Update BreakAPalindrome.cpp

--- BreakAPalindrome.cpp
+++ BreakAPalindrome.cpp
@@ -5,8 +5,6 @@ A string a is lexicographically smaller than a string b (of the same length) if 
 a has a character strictly smaller than the corresponding character in b. 
 For example, "abcc" is lexicographically smaller than "abcd" because the first position they differ is at the fourth character, and 'c' is smaller than 'd'.
 
- 
-
 Example 1:
 Input: palindrome = "abccba"
 Output: "aaccba"
