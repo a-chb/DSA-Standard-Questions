7cddb021c4084d6770f41098eb5fb8eab5f3d9c1
Adarsh Chaturvedi <83339750+a-chb@users.noreply.github.com>
2022-10-10 11:35:50 +0530

    Update BreakAPalindrome.cpp

--- BreakAPalindrome.cpp
+++ BreakAPalindrome.cpp
@@ -19,3 +19,22 @@ Explanation: There is no way to replace a single character to make "a" not a pal
 Constraints:
 1 <= palindrome.length <= 1000
 palindrome consists of only lowercase English letters.
+
+||=======================================>Solution<=======================================||
+  
+class Solution {
+public:
+    string breakPalindrome(string p) {
+        if(p.size()==1)return "";
+        for(int i=0;i<p.size()/2;i++){
+            if(p[i]!='a')
+            {
+                p[i]='a';
+                return p;
+            }
+            
+        }
+        p[p.size()-1]='b';
+        return p;
+    }
+};
